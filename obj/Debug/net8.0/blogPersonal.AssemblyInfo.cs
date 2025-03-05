f0664502e0aa0eebe29a1ae87283d8db5228487b
juan <juanmaf236@gmail.com>
2025-03-04 22:23:26 -0300

    git ignore

--- obj/Debug/net8.0/blogPersonal.AssemblyInfo.cs
+++ obj/Debug/net8.0/blogPersonal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blogPersonal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31998a7ccaed0803b89ac2f597859edc421c952d")]
 [assembly: System.Reflection.AssemblyProductAttribute("blogPersonal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blogPersonal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
